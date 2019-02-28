9e10d130a038ee86fbaee90f2b7ab65fb7423f4c
Zero <jrr043@latech.edu>
2019-02-27 22:22:50 -0600

    Social card MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+      <a className="banner-container" href="https://reactjs.org">
+        <img
+          alt="react logo"
+          className="content-img"
+          src="https://ibin.co/3wnC6SgIOJud.png"
+        />
+      </a>
+    );
+  };
+  
+  export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+      <div className="content-bottom">
+        <p className="content-header">Get started with React</p>
+        <p className="content-block">
+          React makes it painless to make interactive UI's. Design simple views
+          for each state in your application.
+        </p>
+        <a
+          href="http://reactjs.org"
+          className="content-link"
+        >
+          reactjs.org
+        </a>
+      </div>
+    );
+  };
+  export default CardContent;
