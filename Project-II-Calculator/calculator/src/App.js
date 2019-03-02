809eb06468791cd4c10c234b63b0524edf73be14
Zero <jrr043@latech.edu>
2019-03-02 01:02:59 -0600

    Calculator MVP

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
 import './App.css';
+
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-wrapper">
+      <div className="content-row">
+        <CalculatorDisplay />
+      </div>
+      <div className="content-row">
+        <ActionButton name="clear" size="large"/>
+        <ActionButton name="÷" size="small"/>
+      </div>
+      <div className="content-row">
+        <NumberButton name="7" size="small"/>
+        <NumberButton name="8" size="small"/>
+        <NumberButton name="9" size="small"/>
+        <ActionButton name="×" size="small"/>
+      </div>
+      <div className="content-row">
+        <NumberButton name="4" size="small"/>
+        <NumberButton name="5" size="small"/>
+        <NumberButton name="6" size="small"/>
+        <ActionButton name="-" size="small"/>
+      </div>
+      <div className="content-row">
+        <NumberButton name="1" size="small"/>
+        <NumberButton name="2" size="small"/>
+        <NumberButton name="3" size="small"/>
+        <ActionButton name="+" size="small"/>
+      </div>
+      <div className="content-row">
+        <NumberButton name="0" size="large"/>
+        <ActionButton name="=" size="small"/>
+      </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    if (props.name === "0") {
+        return (
+            <div className="number zero">
+                <p className="zero">{props.name}</p>
+            </div>
+        );      
+    }
+    else {
+        return (
+            <div className="number">
+                <p className="small-button">{props.name}</p>
+            </div>
+        );
+    }
+}
+
+export default NumberButton;
